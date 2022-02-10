8c21873bfbdecebec1650e2c11efa1c84c3e8dbc
Dieisileo <dieisileo@gmail.com>
2022-02-10 19:42:54 -0300

    commit

---
+++ public/scripts/script.js
@@ -0,0 +1,8 @@
+const recipes = document.querySelectorAll('.mostPopularRecipes')
+
+for(let recipe of recipes) {
+  recipe.addEventListener('click', function () {
+    const recipeId = recipe.getAttribute('id')
+    window.location.href = `recipes/${recipeId}`
+  })
+}
